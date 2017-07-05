60ec61a7b083c87c635028025747f887c2d0efdf
Sam Tomlinson <samtomlinson@dyn1231-145.wlan.ic.ac.uk>
2017-07-05 14:02:49 +0100

    Added base velocity in place of Gstar

--- baseflow.m
+++ baseflow.m
@@ -1,4 +1,4 @@
-function [x,baseT,baseTdash]= baseflow(C,Pr,D,eta)
+function [x,baseT,baseTdash,baseU]= baseflow(C,Pr,D,eta)
     
 dydx=@(x,z)[z(2);z(3);(-x*z(3)+ (((C+1)*(C-z(4)*z(5)))/(2*sqrt(z(4))*(C+z(4))^2))*z(3))/(((1+C)*sqrt(z(4)))/(z(4)+C)); ...
     z(5);(-x*z(5) + (Pr^-1)*(((C+1)*(C-z(4)*z(5)))/(2*sqrt(z(4))*(C+z(4))^2))*z(5))/((Pr^-1)*((1+C)*sqrt(z(4)))/(z(4)+C))];
@@ -12,4 +12,6 @@ solint=bvpinit(linspace(1,3,1001),zint);
 S=bvp4c(dydx,BC,solint);
     
 x=S.x; baseT=S.y(4,:); baseTdash=S.y(5,:);
+ 
+baseU=S.y(2,:);
     
