8ce3f1ba16b842db8fcb501abc9528c408d847af
Sam Tomlinson <samtomlinson@dyn1231-145.wlan.ic.ac.uk>
2017-07-05 14:11:49 +0100

    Modified Gotler to include the definition of B which is the integral of the base temperature

--- gotler.m
+++ gotler.m
@@ -2,6 +2,6 @@ function vecq = gotler(x,q,baseT,baseTdash,baseU,kappa,betag,Gstar,Q,sigma)
     vecq(1) = q(2);
     vecq(2) = q(2)*baseTdash./baseT ...
         + ((baseT^(-2)).*betag.*q(1) + ...
-        (0.5*kappa*(baseU^2)-Q)*(baseTdash*betag/sigma))*q(1);
+        (0.5*kappa*(baseU^2)-trapz(baseT))*(baseTdash*betag/sigma))*q(1);
     
     
