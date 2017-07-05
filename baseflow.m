3bd4388ae008fd958db3a2490b9ee7f8196db595
Sam Tomlinson <samtomlinson@dyn1231-145.wlan.ic.ac.uk>
2017-07-05 13:28:48 +0100

    Modilfied base flow file so that now it works without giving residual errors

--- baseflow.m
+++ baseflow.m
@@ -2,12 +2,12 @@ function [x,baseT,baseTdash]= baseflow(C,Pr,D,eta)
     
 dydx=@(x,z)[z(2);z(3);(-x*z(3)+ (((C+1)*(C-z(4)*z(5)))/(2*sqrt(z(4))*(C+z(4))^2))*z(3))/(((1+C)*sqrt(z(4)))/(z(4)+C)); ...
     z(5);(-x*z(5) + (Pr^-1)*(((C+1)*(C-z(4)*z(5)))/(2*sqrt(z(4))*(C+z(4))^2))*z(5))/((Pr^-1)*((1+C)*sqrt(z(4)))/(z(4)+C))];
-    
-BC=@(za,zb)[za(1) - D/(eta^(3/Pr)) ; zb(1) ; zb(2); za(4)- (9*(1+C)^2)/(Pr*eta^4); zb(4)];
+
+BC=@(za,zb)[za(1) - D/(eta^(3/Pr)) ; zb(2) ; za(2) + (3/Pr)*D/eta^((3/Pr)-1); za(4)- (9*(1+C)^2)/(Pr^2*eta^4); zb(4)-1];
     
 zint=@(x)[0 ; 1; 0 ; 1 ; 0];
     
-solint=bvpinit(linspace(1,4,1001),zint);
+solint=bvpinit(linspace(1,3,1001),zint);
     
 S=bvp4c(dydx,BC,solint);
     
