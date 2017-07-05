439e722fe82da3964222b03a3e1f140e86cdc311
Sam Tomlinson <samtomlinson@dyn1231-145.wlan.ic.ac.uk>
2017-07-05 14:02:57 +0100

    Added base velocity in place of Gstar

--- gotler.m
+++ gotler.m
@@ -1,7 +1,7 @@
-function vecq = gotler(x,q,baseT,baseTdash,betag,Gstar,Q,sigma)
+function vecq = gotler(x,q,baseT,baseTdash,baseU,kappa,betag,Gstar,Q,sigma)
     vecq(1) = q(2);
     vecq(2) = q(2)*baseTdash./baseT ...
         + ((baseT^(-2)).*betag.*q(1) + ...
-        (Gstar-Q)*(baseTdash*betag/sigma))*q(1);
+        (0.5*kappa*(baseU^2)-Q)*(baseTdash*betag/sigma))*q(1);
     
     
