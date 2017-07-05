86c23d42d81fcc089e0f21a9d6f8e1b6524fba66
Sam Tomlinson <samtomlinson@dyn1231-145.wlan.ic.ac.uk>
2017-07-05 13:12:02 +0100

    couple of small changes

--- gotler.m
+++ gotler.m
@@ -1,6 +1,7 @@
-function vecq = gotler(eta,q,baseT,baseTdash,betag,Gstar,Q,sigma)
+function vecq = gotler(x,q,baseT,baseTdash,betag,Gstar,Q,sigma)
     vecq(1) = q(2);
     vecq(2) = q(2)*baseTdash./baseT ...
-        + ((baseT^(-2)).*betag.*q(1) + (Gstar-Q)*(baseTdash*betag/sigma))*q(1);
+        + ((baseT^(-2)).*betag.*q(1) + ...
+        (Gstar-Q)*(baseTdash*betag/sigma))*q(1);
     
     
