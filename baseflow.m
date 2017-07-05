bd87a470006763d099257a1fa7c4300613fc3b05
Sam Tomlinson <samtomlinson@dyn1231-145.wlan.ic.ac.uk>
2017-07-05 18:55:10 +0100

    Still having problems with the solution but code works. I need to try and sort out the dependence on the initial shoots as it doesnt seem right that solution changes when they change, basically just need to go through the code line by line and work out what is going on and write it out

--- baseflow.m
+++ baseflow.m
@@ -1,9 +1,9 @@
-function [x,baseT,baseTdash,baseU,intbaseT]= baseflow(C,Pr,D,eta)
+function [eta,baseT,baseTdash,baseU,intbaseT]= baseflow(C,Pr,D,etab)
     
-dydx=@(x,z)[z(2);z(3);(-x*z(3)+ (((C+1)*(C-z(4)*z(5)))/(2*sqrt(z(4))*(C+z(4))^2))*z(3))/(((1+C)*sqrt(z(4)))/(z(4)+C)); ...
-    z(5);(-x*z(5) + (Pr^-1)*(((C+1)*(C-z(4)*z(5)))/(2*sqrt(z(4))*(C+z(4))^2))*z(5))/((Pr^-1)*((1+C)*sqrt(z(4)))/(z(4)+C))];
+dydx=@(eta,z)[z(2);z(3);(-eta*z(3)+ (((C+1)*(C-z(4)*z(5)))/(2*sqrt(z(4))*(C+z(4))^2))*z(3))/(((1+C)*sqrt(z(4)))/(z(4)+C)); ...
+    z(5);(-eta*z(5) + (Pr^-1)*(((C+1)*(C-z(4)*z(5)))/(2*sqrt(z(4))*(C+z(4))^2))*z(5))/((Pr^-1)*((1+C)*sqrt(z(4)))/(z(4)+C))];
 
-BC=@(za,zb)[za(1) - D/(eta^(3/Pr)) ; zb(2) ; za(2) + (3/Pr)*D/eta^((3/Pr)-1); za(4)- (9*(1+C)^2)/(Pr^2*eta^4); zb(4)-1];
+BC=@(za,zb)[za(1) - D/(etab^(3/Pr)) ; zb(2) ; za(2) + (3/Pr)*D/etab^((3/Pr)-1); za(4)- (9*(1+C)^2)/(Pr^2*etab^4); zb(4)-1];
     
 zint=@(x)[0 ; 1; 0 ; 1 ; 0];
     
@@ -11,25 +11,21 @@ solint=bvpinit(linspace(1,7,1001),zint);
     
 S=bvp4c(dydx,BC,solint);
     
-x=S.x; baseT=S.y(4,:); baseTdash=S.y(5,:); intbaseT=trapz(baseT);
+eta=S.x; baseT=S.y(4,:); baseTdash=S.y(5,:); intbaseT=trapz(baseT);
  
 baseU=S.y(2,:);
 
 figure('position', [0,0,800,800]); 
-plot(x,baseT,'LineWidth',2); 
+plot(eta,baseT,'LineWidth',2); 
 set(gca,'Fontsize',20)
-%hold on; plot(a2,b2); plot(a3,b3); plot(a4,b4); plot(a5,b5); 
-%legend('n=0.2 Num','n=0.4  Num','n=0.6  Num','n=0.8  Num','n=1.0  Num')
 ylabel('Temp. in adj. region, $T_1$','Interpreter', 'LaTex','Fontsize',40)
 xlabel('Wall layer variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
 xlim([1,7])
 grid on
 
 figure('position', [0,0,800,800]); 
-plot(x,baseU,'LineWidth',2); 
+plot(eta,baseU,'LineWidth',2); 
 set(gca,'Fontsize',20)
-%hold on; plot(a2,c2); plot(a3,c3); plot(a4,c4); plot(a5,c5)
-%legend('n=0.2 Num','n=0.4  Num','n=0.6  Num','n=0.8  Num','n=1.0  Num')
 ylabel('Vel. in adj. region, $U_1$','Interpreter', 'LaTex','Fontsize',40)
 xlabel('Wall layer variable, $\zeta$','Interpreter', 'LaTex','Fontsize',40)
 xlim([1,7])
--- shooting_gotler.m
+++ shooting_gotler.m
@@ -4,13 +4,14 @@
 
 %                           Code description                          %
 
-% This code implements the shooting method for solving 1D boundary value 
-% problem. It uses the Runge-Kutta method of 4th order for solving the 
-% ODE and the interval bisection method for finding the alpha parameter. 
+% This code uses the shooting method for solving 1D gotler stability 
+% equation with Dirichlet BCS. It uses 4th order RK  for solving the 
+% DE and finds the bisection to ensure BCs are satisfied and solution
+% reached.
 
-% The meaning of input and output parameters: 
+% Key: 
 %
-% x - grid of points where the solution have been found
+% eta - grid points
 %
 % y - 2D array, values of function (solution) are in first row, values 
 % of 1st derivative are in second row
@@ -37,7 +38,7 @@
 
 % Example run that works to an extent:
 %
-% [x y,baseT] = shooting_gotler(@gotler,0.0060,1e-6,1,7,[0 0],'ff');
+% [eta, y] = shooting_gotler(@gotler,0.0060,1e-6,1,7,[0 0],'ff');
 %
 % It is meant that will be solved the BVP ODE described in the function
 % gotler, on the interval (1,3) with boundary conditions y(1) = 0 and 
@@ -45,19 +46,19 @@
 
 % Function for output of eigenfuntion
 
-function [x, y, baseT] = shooting_gotler(gotler,h,zero,a,b,con,...
+function [eta, y] = shooting_gotler(gotler,h,zero,a,b,con,...
     type,init) 
 
     % Parameters and base flow should really be put into funtion 
 
     gamma=1.4; Pr=1; C=0.509;
     D=1; % Fitting parameter for base flow 
-    eta=1; % Chosen matching point or left boundary 
-    betag=1; Gstar=2; Q=1; sigma=0.1; kappa=1;
+    etab=1; % Chosen matching point or left boundary 
+    betag=1; sigma=0.1; kappa=1;
     
     % Solve for the base flow 
     
-    [~,baseT,baseTdash,baseU,intbaseT]= baseflow(C,Pr,D,eta);
+    [~,baseT,baseTdash,baseU,intbaseT]= baseflow(C,Pr,D,etab);
 
     tic; % Begin time
     
@@ -84,9 +85,9 @@ function [x, y, baseT] = shooting_gotler(gotler,h,zero,a,b,con,...
     % Now iterate solution outwards using Rk method 
     
     [~, F1] = RK(a,b,h,a1,gotler,baseT,baseTdash,baseU,kappa,betag,...
-        Gstar,Q,sigma,intbaseT); 
-    [x, F2] = RK(a,b,h,a2,gotler,baseT,baseTdash,baseU,kappa,betag,...
-        Gstar,Q,sigma,intbaseT);         
+        sigma,intbaseT); 
+    [eta, F2] = RK(a,b,h,a2,gotler,baseT,baseTdash,baseU,kappa,betag,...
+        sigma,intbaseT);         
     
     if (type(2)=='f')
         F1 = F1(1,end) - con(2);
@@ -96,7 +97,7 @@ function [x, y, baseT] = shooting_gotler(gotler,h,zero,a,b,con,...
         F1 = F1(2,end) - con(2); 
         F2 = F2(2,end) - con(2);
         r = 2;
-    end    
+    end   
 
     % Identify if as root is possible by checking for sign change
     
@@ -112,7 +113,8 @@ function [x, y, baseT] = shooting_gotler(gotler,h,zero,a,b,con,...
     
     while (abs(F3) > zero) 
         
-        F3;
+        % Check
+        % F3;
         
         % Bring one shoot in half the distance between the teo
         
@@ -127,8 +129,11 @@ function [x, y, baseT] = shooting_gotler(gotler,h,zero,a,b,con,...
            a3 = [shoot3 con(1)];            
         end           
         
-        [x, F3] = RK(a,b,h,a3,gotler,baseT,baseTdash,baseU,kappa,...
-            betag,Gstar,Q,sigma,intbaseT);
+        [eta, F3] = RK(a,b,h,a3,gotler,baseT,baseTdash,baseU,kappa,...
+            betag,sigma,intbaseT);
+        
+        % Check
+        % F3(r,end);
         
         y = F3; F3 = F3(r,end) - con(2); 
         if (F1*F3 < 0)
@@ -143,9 +148,11 @@ function [x, y, baseT] = shooting_gotler(gotler,h,zero,a,b,con,...
     
     % Plotting of solutions 
     
+    size(eta)
+    
     figure('position', [0,0,800,800]); 
-    plot(x,y(1,:),'k-','LineWidth',2); hold on; 
-    plot(x,y(2,:),'r-','LineWidth',2); 
+    plot(eta,y(1,:),'k-','LineWidth',2); hold on; 
+    plot(eta,y(2,:),'r-','LineWidth',2); 
     set(gca,'Fontsize',20)
     l1=legend('$v_0(\eta)$','$v_{0\eta}(\eta)$');
     set(l1, 'Interpreter','LaTex','Fontsize',30);
