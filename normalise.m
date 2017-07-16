90ceff23662e701d1338c8567464bcd19bb9e83a
SamTomlinson <sdt16@ic.ac.uk>
2017-07-16 14:33:31 +0100

    adding normalisation function

---
+++ normalise.m
@@ -0,0 +1,20 @@
+function [vn] = ...
+          normalise(v)
+
+maxs=[];
+for j=2:length(v(1,:))-1
+    if (v(1,j-1)<v(1,j) && v(1,j)>v(1,j+1))
+        maxs=[j,maxs];
+    end
+    if (v(1,j-1)<v(1,j) && v(1,j)>v(1,j+1))
+        maxs=[j,maxs];
+    end
+    if (v(1,j-1)<v(1,j) && v(1,j)>v(1,j+1))
+        maxs=[j,maxs];
+    end
+    
+end
+
+% Normalise
+
+v(1,:)=(1/v(1,maxs(1)))*v(1,:);
