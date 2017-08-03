f40b486caf8eac053415c5b123ac77dd8eba560e
SamTomlinson <sdt16@ic.ac.uk>
2017-08-03 09:30:35 +0100

    tidied up code ready for checking

--- shooting_gotler3.m
+++ shooting_gotler3.m
@@ -2,218 +2,85 @@
 %                           shooting_gotler                           %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
-%                           Code description                          %
-
-
-
 % Shooting method for solving 1D gotler stability equation with 
-% Dirichlet BCS. 4th order RK and bisection to ensure BCs
-
-
-
-
-%                                 Key                                 % 
-%
-% eta - grid points
-%
-% v - v0 and v0dash array 
-%
-% gotler - function containing de for gotler
-%
-% deltaeta - step size
-%
-% tol - tolerance
-%
-% bcs - values of boundary conditions (2D vector)
-%
-% init - initial guess, if not specified given as [-5,10]
-%
-% a,b - two ends of the domain
-%
-% flow parameters - gamma (specific heat), Pr (prandtl), C (sutherlands
-% constant), D (fitting parameter), etab (matching point for edge of
-% adjustment region)
-% 
-% eigval - found eigenvalue for entered khat
-%
-% k - spanwise wavenumber
-% 
-% base flow - baseT, baseTdash, base U base flow vectors and derivatives
-% intbaseT integral for Q term in DE
-
-
-
-%                         Output eigenfuntion                          %
-
-
+% Dirichlet asymptotic BCS. Uses a 4th order RK and bisection method
+% to ensure BCs need very accurate shoot. Outputs nodes, eigenmodes
+% and the shoot value
 
 function [eta, v,eigval] = shooting_gotler3(gotler,deltaeta,a,b,khat) 
 
-    % Parameters and base flow should really be put into funtion 
+% parameters and base flow should really be put into funtion 
+Pr=1; C=0.509; D=1; A=3*(1+C)/Pr;
+% solve for the base flow 
+[~,baseT,baseTdash,~,~]=baseflow(C,Pr,D,deltaeta,a,b);
+% initialise vectors
+vec=[]; eigvec=[];
+% loop through different khat values 
+for shoot1=0.01:0.001:0.6
+    % boundary conditions 
+    a1 = [exp(-khat*b), -khat*exp(-khat*b)];
+    % runge kutta inwards
+    [~, F1] = RK(a,b,deltaeta,a1,gotler,baseT,baseTdash,khat,shoot1);
+    % boundary condition constraint
+    H1=F1(2,1)-((khat*A^2)/(a^4))*F1(1,1);
+    % vector of H error and ks
+    vec=[H1,vec]; eigvec=[shoot1,eigvec];
+end  
 
-    Pr=1; C=0.509; D=1; A=3*(1+C)/Pr; tol=1e-6;
-    
-    % Solve for the base flow 
-    
-    [~,baseT,baseTdash,~,~]=baseflow(C,Pr,D,deltaeta,a,b);
+% plot H vs eig  
+figure('position', [0,0,800,800]); 
+plot(eigvec,vec,'k-','LineWidth',2); 
+set(gca,'Fontsize',20)
+ylabel('Near field error, $H(\hat{k})$','Interpreter',...
+        'LaTex','Fontsize',40)
+xlabel('Eig. val., $\beta^2(G^*-Q)$','Interpreter', 'LaTex','Fontsize',40)
+xlim([0.01,0.6])
+grid on
+hold off;  
 
-    % Begin time
-    
-    tic;
-    
-    % Initialise vectors
-    
-    vec=[]; eigvec=[];
-    
-    % Loop through different khat values 
-    
-    for shoot1=0.01:0.001:0.6
-        
-        % Far field boudary condition 
-        
-        a1 = [exp(-khat*b), -khat*exp(-khat*b)];
-        a2 = [exp(-khat*A^2/(3*a^3)), khat*A^2/(a^4)*exp(-khat*A^2/(3*a^3))];
-        
-        % Runge kutta inwards
-        
-        [~, F1] = RK(a,b,deltaeta,a1,gotler,baseT,baseTdash,khat,shoot1);
-        %[~, F1] = RK(a,b,deltaeta,a2,gotler,baseT,baseTdash,khat,shoot1);
-    
-        % Boundary condition constraints
-        
-        H1=F1(2,1)-((khat*A^2)/(a^4))*F1(1,1);
-        
-        H2=F1(2,end) + khat*F1(1,end);
-   
-        % Vector of H error and ks
-        
-        vec=[H1,vec]; eigvec=[shoot1,eigvec];
-    
-    end
-    
-    
-% Plot H vs eig
-    
-%     figure('position', [0,0,800,800]); 
-%     plot(eigvec,vec,'k-','LineWidth',2); 
-%     set(gca,'Fontsize',20)
-%     ylabel('Near field error, $H(\hat{k})$','Interpreter',...
-%         'LaTex','Fontsize',40)
-%     xlabel('Eig. val., $\beta^2(G^*-Q)$','Interpreter', 'LaTex','Fontsize',40)
-%     xlim([0.01,0.6])
-%     %ylim([-1,1])
-%     grid on
-%     hold off;
-   
-% Calculate the crossing points
-    
+% calculate index of the crossing points  
 zerIdx=[];
 for i=1:length(vec)-1
     if ((vec(i)>0 && vec(i+1)<0) || (vec(i)<0 && vec(i+1)>0))
-    zerIdx(end+1)=i; % save index of zero-crossing
+    zerIdx(end+1)=i;
     end
 end
-
-% Improve accuracy 
-
-eigs=eigvec(zerIdx);
-vecs=vec(zerIdx);
-% eigvalright=eigvec(zerIdx(1)-1);
-% eigvalleft=eigvec(zerIdx(1)+1);
-% vecsright=vec(zerIdx(1)-1);
-% vecsleft=vec(zerIdx(1)+1);
-eigval=eigs(1);
-
-
-% eigvalright=0.342; eigvalleft=0.34;  vecsright=-1.426036221091628e+08;
-% vecsleft=1.677088912688837e+06;
-
-% Iterate in 
-
-% while (vecsleft>1e-16)
-% eignew=(eigvalleft+eigvalright)/2;
-% vecnew=(vecsleft+vecsright)/2;
-% if vecnew<0
-%     eigvalright=eignew;
-%     vecsright=vecnew;
-% end
-% if vecnew>0
-%     eigvalleft=eignew;
-%     vecsleft=vecnew;
-% end
-% end
-% 
-% eigval=eigvalleft;
-
-% Improve accuracy 
-diff=1;
-
-tol=0.1;
+% improve accuracy 
+eigs=eigvec(zerIdx); eigval=eigs(1);
+diff=1; tol=0.1;
 while abs(diff>1e-16)
     eigvalold=eigval;
-    [eigval,H1]=loop(eigval,khat,a,b,A,deltaeta,a1,gotler,baseT,...
+    [eigval,~]=loop(eigval,khat,a,b,A,deltaeta,a1,gotler,baseT,...
     baseTdash,shoot1,tol);
     diff=abs(eigvalold-eigval);
     tol=tol/10;
-end
-
-
-       
-% Calculation of eigenmodes 
-    
+end      
+% calculation of eigenmodes with accuarate khat    
 a1 = [exp(-khat*b), -khat*exp(-khat*b)];
 [eta, F1] = RK(a,b,deltaeta,a1,gotler,baseT,baseTdash,khat,eigval);     
-H1=F1(2,1)-((khat*A^2)/(a^4))*F1(1,1);
-H2=F1(2,length(F1(2,:))) + khat*F1(1,length(F1(2,:)));
 v=F1;
-
-% Set all values below zero to zero (not sure if this is allowed)
-
-v1=v(1,:);
-v1(v1<0)=0;
-v(1,:)=v1;
-
-% Find local maxima
-
+% find local maxima
 maxs=[];
 for j=2:length(v(1,:))-1
     if (v(1,j-1)<v(1,j) && v(1,j)>v(1,j+1))
         maxs=[j,maxs];
     end
 end
-
-% Normalise
-
+% normalise
 v(1,:)=(1/v(1,maxs(1)))*v(1,:);
+% eliminate deviation
+[~,b1]=find(v(1,:) == 1);
+[~,b2]=min(v(1,2:b1));
+v(:,1:b2)=0;
 
-
-% Normalisation of eigenmodes
-
-
-
-% Plotting of eigenomdes (if running evvsk % out)
-
-%     figure('position', [0,0,800,800]); 
-%     plot(eta,v(1,:),'LineWidth',2); 
-%     set(gca,'Fontsize',20)
-%     ylabel('Vel. in the temp. adj. region $v_0$','Interpreter',...
-%         'LaTex','Fontsize',40)
-%     xlabel('D.H. variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
-%     xlim([0.1,b])
-%     grid on
-%     hold off;
-%     
-%     figure('position', [0,0,800,800]); 
-%     plot(eta,-baseTdash.*v(1,:)./baseT,'k-','LineWidth',2);
-%     set(gca,'Fontsize',20)
-%      ylabel('Temp. in the temp. adj. region $T_0$','Interpreter',...
-%         'LaTex','Fontsize',40)
-%     xlabel('D.H. variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
-%     xlim([0.1,b])
-%     grid on
-%     hold off;
-    toc
-    
+% plotting of eigenmodes (if running evvsk % out)
+figure('position', [0,0,800,800]); 
+plot(eta,v(1,:),'LineWidth',2); 
+set(gca,'Fontsize',20)
+ylabel('Vel. in the temp. adj. region $v_0$','Interpreter',...
+         'LaTex','Fontsize',40)
+xlabel('D.H. variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
+xlim([0.1,b])
+grid on
+hold off;   
 end
