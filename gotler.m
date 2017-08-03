213b8ab70c6d1153e156880b3a2b43630eb21e43
SamTomlinson <sdt16@ic.ac.uk>
2017-08-03 10:09:27 +0100

    tidied up code ready for checking

--- gotler.m
+++ gotler.m
@@ -2,47 +2,18 @@
 %                               gotler                                %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
-%                           Code description                          %
-
-
-
-% Encode gotler system
-
-
-
-
-%                                 Key                                 % 
-%
-% eta - grid points
-%
-% q - derivatives v_0 and v_0'
-%
-% base flow - baseT, baseTdash, base U base flow vectors and derivatives
-% intbaseT integral for Q term in DE
-%
-% khat - spanwise wavenumber
-%
-% ev - eigen value 
-%
-
-
-
-%                          Gotler system                               %
-
-
+% Encodes the gotler differential equation. Outputs the solution 
+% vector of eigenmode at node and its derivative
 
 function vecq = gotler(~,q,baseT,baseTdash,khat,ev)
     
-% Diff of q1 is q2 
-
-vecq(1) = q(2);
-    
-% Diff of q2 is the rest of the system
-
-vecq(2) =  (2.*q(2).*baseTdash)./(baseT) ...
+    % differential of q1 is q2 
+    vecq(1) = q(2);    
+    % differential of q2 is the rest of the system
+    vecq(2) =  (2.*q(2).*baseTdash)./(baseT) ...
         + (baseT.^2).*(khat.^2).*q(1) ...
         + (1/ev).*baseTdash.*(khat.^2).*q(1);
+    
+end
     
     
