69b9ebb9cf7d5502efd9d03fb6ca11a904e57c62
SamTomlinson <sdt16@ic.ac.uk>
2017-08-03 10:20:34 +0100

    tidied up code ready for checking

--- evvsk.m
+++ evvsk.m
@@ -2,81 +2,26 @@
 %                                 evvsk                               %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Runs shooting_gotler3 for a variety of different wavenumbers then
+% plots the dependance of the ev on k and some of the different 
+% eigenmodes
 
+%% calculate wavenumber dependancy 
 
-%                           Code description                          %
-
-
-
-% Runs shooting_gotler3 for a variety of different wavenumbers the plots
-% the dependance of the ev on k. Plots some of the different eigenmodes
-
-
-
-%                                 Key                                 % 
-%
-% eta - grid points
-%
-% v - v0 and v0dash array 
-%
-% gotler - function containing de for gotler
-%
-% deltaeta - step size
-%
-% tol - tolerance
-%
-% bcs - values of boundary conditions (2D vector)
-%
-% init - initial guess, if not specified given as [-5,10]
-%
-% a,b - two ends of the domain
-%
-% flow parameters - gamma (specific heat), Pr (prandtl), C (sutherlands
-% constant), D (fitting parameter), etab (matching point for edge of
-% adjustment region)
-% 
-% eigval - found eigenvalue for entered khat
-%
-% k - spanwise wavenumber
-% 
-% base flow - baseT, baseTdash, base U base flow vectors and derivatives
-% intbaseT integral for Q term in DE
-
-
-
-%                           Mostly plotting                           %
-
-
-
-% Flow parameters 
-
+% flow parameters 
 a=0.5; b=20; deltaeta=0.01;
 Pr=1; C=0.509; D=1; A=3*(1+C)/Pr;
-
-% Base flow
-
+% base flow
 [~,baseT,baseTdash,baseU,baseUdash]=baseflow(C,Pr,D,deltaeta,a,b);
-
-%Initialise 
-
+%initialise 
 ev=[];
-
-% Loop through different wavenumbers and calculate ev
-
-for k=0.5:0.5:8
-    
-    k
-    
-    % Calculate ev
-    
+% loop through different wavenumbers and calculate ev
+for k=0.1:0.5:8
+    % calculate ev
     [eta, v,eigval] = shooting_gotler3(@gotler,deltaeta,a,b,k);
-    
-    % Fill vector
-    
+    % fill vector
     ev=[eigval, ev];
-
-    % Extract ones to plot
-    
+    % extract ones to plot
     if k==0.5
         v1=v(1,:);
         dv1=v(2,:);
@@ -90,20 +35,17 @@ for k=0.5:0.5:8
         dv3=v(2,:);
     end     
 end
-
-% Reverse ev order as come out the wrong way round
-
+% reverse ev order as come out the wrong way round
 ev=ev(end:-1:1);
 
-%% Normalisation of other flow variables
+%% normalisation of other flow variables
 
-% Recalculate base eta and base flow variables with correct size
+% recalculate base eta and base flow variables with correct size
 baseeta=a:deltaeta/5:b;
 baseTm = interp1(baseeta,baseT,a:deltaeta:b,'spline');
 baseTdashm = interp1(baseeta,baseTdash,a:deltaeta:b,'spline');
 baseUm = interp1(baseeta,baseU,a:deltaeta:b,'spline');
 baseUdashm = interp1(baseeta,baseUdash,a:deltaeta:b,'spline');
-
 T1=normalise(-baseTdashm.*v1./baseTm);
 T2=normalise(-baseTdashm.*v2./baseTm); 
 T3=normalise(-baseTdashm.*v3./baseTm);
@@ -117,8 +59,7 @@ p1=normalise(-dv1./(0.5.^2.*baseTm.^2));
 p2=normalise(-dv2./(1.^2.*baseTm.^2)); 
 p3=normalise(-dv3./(2.^2.*baseTm.^2));
 
-
-%% Plotting
+%% plotting
 
 figure('position', [0,0,800,800]); 
 plot(baseeta,baseT,'LineWidth',2); 
@@ -127,7 +68,6 @@ ylabel('Temp. in adj. region, $T_1$','Interpreter', 'LaTex','Fontsize',40)
 xlabel('Wall layer variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
 xlim([0,b])
 grid on
-
 figure('position', [0,0,800,800]); 
 plot(baseeta,baseU,'LineWidth',2); 
 set(gca,'Fontsize',20)
@@ -135,10 +75,8 @@ ylabel('Vel. in adj. region, $U_1$','Interpreter', 'LaTex','Fontsize',40)
 xlabel('Wall layer variable, $\zeta$','Interpreter', 'LaTex','Fontsize',40)
 xlim([0,b])
 grid on
-
-
 figure('position', [0,0,800,800]); 
-plot(0.5:0.5:8,ev); hold on;
+plot(0.1:0.5:8,ev); hold on;
 set(gca,'Fontsize',20)
 ylabel('Eigenvalue, $\beta^2(G^*-Q)$','Interpreter',...
         'LaTex','Fontsize',40)
@@ -146,7 +84,6 @@ xlabel('Wavenumber, $\hat{k}$','Interpreter', 'LaTex','Fontsize',40)
 xlim([0.5,8])
 grid on
 hold off;
-
 figure('position', [0,0,800,800]); 
 plot(eta,v1,'LineWidth',2); hold on; 
 plot(eta,v2,'LineWidth',2);  
@@ -155,63 +92,6 @@ set(gca,'Fontsize',20)
 l1=legend('$k=0.5$','$k=1$','$k=2$');
 set(l1, 'Interpreter','LaTex','Fontsize',30);
 ylabel('Vel. in the temp. adj. region $v_0$','Interpreter',...
-        'LaTex','Fontsize',40)
-xlabel('D.H. variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
-xlim([0,b])
-
-grid on
-hold off;
-    
-figure('position', [0,0,800,800]); 
-plot(eta,T1,'LineWidth',2); hold on; 
-plot(eta,T2,'LineWidth',2); 
-plot(eta,T3,'LineWidth',2); 
-set(gca,'Fontsize',20)
-l1=legend('$k=0.5$','$k=1$','$k=2$');
-set(l1, 'Interpreter','LaTex','Fontsize',30);
-ylabel('Temp. in the temp. adj. region $T_0$','Interpreter',...
-        'LaTex','Fontsize',40)
-xlabel('D.H. variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
-xlim([0,b])
-grid on
-hold off;
-
-figure('position', [0,0,800,800]); 
-plot(eta,u1,'LineWidth',2); hold on; 
-plot(eta,u2,'LineWidth',2); 
-plot(eta,u3,'LineWidth',2); 
-set(gca,'Fontsize',20)
-l1=legend('$k=0.5$','$k=1$','$k=2$');
-set(l1, 'Interpreter','LaTex','Fontsize',30);
-ylabel('Vel. in the temp. adj. region $u_0$','Interpreter',...
-        'LaTex','Fontsize',40)
-xlabel('D.H. variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
-xlim([0,b])
-grid on
-hold off;
-
-figure('position', [0,0,800,800]); 
-plot(eta,w1,'LineWidth',2); hold on; 
-plot(eta,w2,'LineWidth',2); 
-plot(eta,w3,'LineWidth',2); 
-set(gca,'Fontsize',20)
-l1=legend('$k=0.5$','$k=1$','$k=2$');
-set(l1, 'Interpreter','LaTex','Fontsize',30);
-ylabel('Vel. in the temp. adj. region $w_0$','Interpreter',...
-        'LaTex','Fontsize',40)
-xlabel('D.H. variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
-xlim([0,b])
-grid on
-hold off;
-
-figure('position', [0,0,800,800]); 
-plot(eta,p1,'LineWidth',2); hold on; 
-plot(eta,p2,'LineWidth',2); 
-plot(eta,p3,'LineWidth',2); 
-set(gca,'Fontsize',20)
-l1=legend('$k=0.5$','$k=1$','$k=2$');
-set(l1, 'Interpreter','LaTex','Fontsize',30);
-ylabel('Pres. in the temp. adj. region $p_0$','Interpreter',...
         'LaTex','Fontsize',40)
 xlabel('D.H. variable, $\eta$','Interpreter', 'LaTex','Fontsize',40)
 xlim([0,b])
